88697645ec07d94d4b3017413e076ab95d3a0bd9
Amar Kumar <amarkumar029@gmail.com>
2025-04-14 13:41:11 +0530

    latest update

--- src/navigation/base.js
+++ src/navigation/base.js
@@ -1,4 +1,4 @@
-// const baseUrl = "https://kisancollege.co.in/nounodeapi";
-const baseUrl = "http://localhost:8000/nodeapi";
+const baseUrl = "https://ssinhacollege.com/nodeapi";
+// const baseUrl = "http://localhost:8000/nodeapi";
 
 export default baseUrl;
